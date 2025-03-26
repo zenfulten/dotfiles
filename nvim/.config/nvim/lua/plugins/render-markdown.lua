de8602935bb377c15225f339249bd8555019bcb2
Rio <rioten353@gmail.com>
2025-03-26 18:55:32 +0530

    render-markdown configure

--- nvim/.config/nvim/lua/plugins/render-markdown.lua
+++ nvim/.config/nvim/lua/plugins/render-markdown.lua
@@ -2,5 +2,27 @@ return {
 	"MeanderingProgrammer/render-markdown.nvim",
 	opts = {
 		heading = { position = "inline", width = "block" },
+		checkbox = {
+			checked = { icon = "✔ ", scope_highlight = "@markup.strikethrough" },
+			unchecked = { icon = "✘ " },
+			custom = {
+				important = {
+					raw = "[~]",
+					rendered = "󰓎 ",
+					highlight = "DiagnosticWarn",
+				},
+			},
+		},
+		code = { style = "language", width = "block" },
+		pipe_table = { preset = "round" },
+		link = {
+			email = " ",
+			custom = {
+				python = {
+					pattern = "%.py$",
+					icon = "󰌠 ",
+				},
+			},
+		},
 	},
 }
