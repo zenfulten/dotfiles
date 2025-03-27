2865521c9c2472fc6c8c57153000c18099919136
Rio <rioten353@gmail.com>
2025-03-27 14:47:07 +0530

    config alpha

--- nvim/.config/nvim/lua/plugins/alpha.lua
+++ nvim/.config/nvim/lua/plugins/alpha.lua
@@ -1,31 +1,33 @@
 return {
 	"goolord/alpha-nvim",
 	event = "VimEnter",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
-
 	config = function()
 		local alpha = require("alpha")
-		local dashboard = require("alpha.themes.startify")
+		local dashboard = require("alpha.themes.dashboard")
 
+		-- Set header
 		dashboard.section.header.val = {
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                     ]],
-			[[       ████ ██████           █████      ██                     ]],
-			[[      ███████████             █████                             ]],
-			[[      █████████ ███████████████████ ███   ███████████   ]],
-			[[     █████████  ███    █████████████ █████ ██████████████   ]],
-			[[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
-			[[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
-			[[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
+
+			"                                                           ",
+			"   ██████╗ ██╗███╗   ██╗██████╗ ███████╗███████╗██╗  ██╗   ",
+			"   ██╔══██╗██║████╗  ██║██╔══██╗██╔════╝██╔════╝██║  ██║   ",
+			"   ██████╔╝██║██╔██╗ ██║██║  ██║█████╗  ███████╗███████║   ",
+			"   ██╔══██╗██║██║╚██╗██║██║  ██║██╔══╝  ╚════██║██╔══██║   ",
+			"   ██████╔╝██║██║ ╚████║██████╔╝███████╗███████║██║  ██║   ",
+			"   ╚═════╝ ╚═╝╚═╝  ╚═══╝╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═╝   ",
+			"                                                           ",
 		}
+
+		-- Set menu
+		dashboard.section.buttons.val = {
+			dashboard.button("n", "  > New File", "<cmd>ene<CR>"),
+			dashboard.button("SPC e", "  > Toggle file explorer", "<cmd>Neotree filesystem reveal left<CR>"),
+			dashboard.button("SPC fp", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
+			dashboard.button("SPC fg", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
+			dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
+		}
+
+		-- Send config to alpha
 		alpha.setup(dashboard.opts)
 
 		-- Disable folding on alpha buffer
