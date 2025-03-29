912028c69b05048b3843ef6118424218730019bd
Rio <rioten353@gmail.com>
2025-03-29 11:16:21 +0530

    config blink cmp and codeium

--- nvim/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -1,16 +1,33 @@
 return {
 	"saghen/blink.cmp",
+	dependencies = {
+		{ "saghen/blink.compat", opts = { enable_events = true } },
+		{
+			"Exafunction/codeium.nvim",
+			dependencies = {
+				"nvim-lua/plenary.nvim",
+			},
+			opts = {},
+		},
+	},
 	lazy = false, -- lazy loading handled internally
 	version = "v0.*",
 	---@module 'blink.cmp'
 	opts = {
 		keymap = { preset = "default" },
 		appearance = {
-			use_nvim_cmp_as_default = true,
+			use_nvim_cmp_as_default = false,
 			nerd_font_variant = "mono",
 		},
 		sources = {
-			default = { "lsp", "path", "snippets", "buffer" },
+			default = { "codeium", "lsp", "path", "snippets", "buffer" },
+			providers = {
+				codeium = {
+					name = "codeium",
+					module = "blink.compat.source",
+					score_offset = 3,
+				},
+			},
 		},
 		completion = {
 			menu = {
