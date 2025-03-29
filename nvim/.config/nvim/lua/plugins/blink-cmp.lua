4dc1c92cd754d16343abebe9e79db8ff5ef821cf
Rio <rioten353@gmail.com>
2025-03-29 11:57:43 +0530

    config blink cmp

--- nvim/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -33,27 +33,13 @@ return {
 			menu = {
 				border = "single",
 				draw = {
-					components = {
-						kind_icon = {
-							text = function(ctx)
-								local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
-								return kind_icon
-							end,
-							highlight = function(ctx)
-								local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
-								return hl
-							end,
-						},
-						kind = {
-							highlight = function(ctx)
-								local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
-								return hl
-							end,
-						},
+					columns = {
+						{ "label", "label_description", gap = 1 },
+						{ "kind_icon", "kind", gap = 1 },
 					},
 				},
 			},
-			documentation = { window = { border = "single" } },
+			documentation = { auto_show = true, auto_show_delay_ms = 500, window = { border = "single" } },
 		},
 		signature = { enabled = true, window = { border = "single" } },
 	},
