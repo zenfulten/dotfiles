0f305db4268d3cbf695fc9a827d033debb3fb6d1
Rio <rioten353@gmail.com>
2025-03-29 12:08:15 +0530

    config blink cmp

--- nvim/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -16,8 +16,38 @@ return {
 	opts = {
 		keymap = { preset = "default" },
 		appearance = {
-			use_nvim_cmp_as_default = false,
-			nerd_font_variant = "mono",
+			kind_icons = {
+				Text = "󰉿",
+				Method = "󰊕",
+				Function = "󰊕",
+				Constructor = "󰒓",
+
+				Field = "󰜢",
+				Variable = "󰆦",
+				Property = "",
+
+				Class = "󱡠",
+				Interface = "󱡠",
+				Struct = "󱡠",
+				Module = "󰅩",
+
+				Unit = "󰪚",
+				Value = "󰦨",
+				Enum = "󰦨",
+				EnumMember = "󰦨",
+
+				Keyword = "󰻾",
+				Constant = "󰏿",
+
+				Snippet = "󱄽",
+				Color = "󰏘",
+				File = "󰈔",
+				Reference = "󰬲",
+				Folder = "󰉋",
+				Event = "󱐋",
+				Operator = "󰪚",
+				TypeParameter = "󰬛",
+			},
 		},
 		sources = {
 			default = { "codeium", "lsp", "path", "snippets", "buffer" },
