958cf039d0ea171f88a94fd23b3471ce02d94a33
Rio <rioten353@gmail.com>
2025-03-30 10:05:23 +0530

    confing tokyonight

--- nvim/.config/nvim/lua/plugins/tokyonight.lua
+++ nvim/.config/nvim/lua/plugins/tokyonight.lua
@@ -3,6 +3,9 @@ return {
 	lazy = false,
 	priority = 1000,
 	config = function()
-		vim.cmd("colorscheme tokyonight")
+		-- vim.cmd("colorscheme tokyonight-storm")
+		-- vim.cmd("colorscheme tokyonight-moon")
+		-- vim.cmd("colorscheme tokyonight-day")
+		vim.cmd("colorscheme tokyonight-night")
 	end,
 }
