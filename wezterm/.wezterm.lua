bdf1a0573f378d5bf673f2c865b4b09af2ee0f3f
Rio <rioten353@gmail.com>
2025-03-30 09:23:21 +0530

    config wezterm add font

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,43 +1,22 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
--- config.color_scheme = "Tokyo Night"
 config.color_scheme = "Tokyo Night"
--- my coolnight colorscheme
--- config.colors = {
--- 	foreground = "#CBE0F0",
--- 	background = "#011423",
--- 	cursor_bg = "#47FF9C",
--- 	cursor_border = "#47FF9C",
--- 	cursor_fg = "#011423",
--- 	selection_bg = "#033259",
--- 	selection_fg = "#CBE0F0",
--- 	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
--- 	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
--- }
 
--- wezterm.font("FiraMono Nerd Font Mono", { weight = "Bold", italic = false })
-config.font = wezterm.font("MesloLGS Nerd Font Mono")
-config.font = wezterm.font_with_fallback({
-	{ family = "CaskaydiaCove Nerd Font" },
-})
+config.font = wezterm.font("JetBrains Mono", { weight = "Bold", italic = true })
 
 config.font_size = 13
 
 config.enable_tab_bar = false
 
--- config.window_decorations = "RESIZE"
 config.window_decorations = "NONE"
 config.window_background_opacity = 0.75
 config.macos_window_background_blur = 10
 
 config.line_height = 1.0
 
---margin and padding
 config.window_padding = {
 	left = "0px",
 	right = "0px",
@@ -45,10 +24,6 @@ config.window_padding = {
 	bottom = "0px",
 }
 
---cursour
 default_cursor_style = "SteadyBar"
-default_cursor_blink_rate = 0
-default_cursor_width = "1.5px"
 
--- and finally, return the configuration to wezterm
 return config
