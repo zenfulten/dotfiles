1589ffb4943b4ba0ad2d17e2232cf497c9147a0d
Rio <rioten353@gmail.com>
2025-03-31 12:16:15 +0530

    confing transparent background and set tokyonight theme in options.lua

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -2,6 +2,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("colorscheme tokyonight-night")
 
 local opt = vim.opt
 local o = vim.o
--- nvim/.config/nvim/lua/plugins/tokyonight.lua
+++ nvim/.config/nvim/lua/plugins/tokyonight.lua
@@ -2,10 +2,17 @@ return {
 	"folke/tokyonight.nvim",
 	lazy = false,
 	priority = 1000,
-	config = function()
-		-- vim.cmd("colorscheme tokyonight-storm")
-		-- vim.cmd("colorscheme tokyonight-moon")
-		-- vim.cmd("colorscheme tokyonight-day")
-		vim.cmd("colorscheme tokyonight-night")
-	end,
+	opts = {
+		transparent = true,
+		styles = {
+			sidebars = "transparent",
+			floats = "transparent",
+		},
+	},
+	-- config = function()
+	-- 	-- vim.cmd("colorscheme tokyonight-storm")
+	-- 	-- vim.cmd("colorscheme tokyonight-moon")
+	-- 	-- vim.cmd("colorscheme tokyonight-day")
+	-- 	vim.cmd("colorscheme tokyonight-night")
+	-- end,
 }
