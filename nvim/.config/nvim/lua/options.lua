f4e0aad12ac39b355b15455b24c523a75b0954d0
Rio <rioten353@gmail.com>
2025-04-03 18:08:22 +0530

    change options

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -87,6 +87,6 @@ g.loaded_perl_provider = 0
 g.loaded_ruby_provider = 0
 
 g.mapleader = " "
-g.background = "light"
+g.background = "dark"
 
 vim.wo.number = true
--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -8,6 +8,7 @@ return {
 				lua = { "stylua" },
 				dart = { "dartls" },
 				go = { "goimports", "gofumpt" },
+				python = { "ruff" },
 				-- css = { "prettier" },
 				-- html = { "prettier" },
 			},
