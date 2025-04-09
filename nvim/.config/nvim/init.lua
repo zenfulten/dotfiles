89c8211037cf6bf2b222941e481721f88d6c5dd9
Rio <rioten353@gmail.com>
2025-04-09 08:17:05 +0530

    change init modify lazy config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -11,7 +11,50 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+-- Setup lazy.nvim
+require("lazy").setup({
+	spec = {
+		{ import = "plugins" },
+	},
+	ui = {
+		size = { width = 0.8, height = 0.8 },
+		wrap = true, -- wrap the lines in the ui
+		-- The border to use for the UI window. Accepts same border values as |nvim_open_win()|.
+		border = "rounded",
+		-- The backdrop opacity. 0 is fully opaque, 100 is fully transparent.
+		backdrop = 60,
+		title = nil, ---@type string only works when border is not "none"
+		title_pos = "center", ---@type "center" | "left" | "right"
+		-- Show pills on top of the Lazy window
+		pills = true, ---@type boolean
+		icons = {
+			cmd = " ",
+			config = "",
+			debug = "● ",
+			event = " ",
+			favorite = " ",
+			ft = " ",
+			init = " ",
+			import = " ",
+			keys = " ",
+			lazy = "󰒲 ",
+			loaded = "●",
+			not_loaded = "○",
+			plugin = " ",
+			runtime = " ",
+			require = "󰢱 ",
+			source = " ",
+			start = " ",
+			task = "✔ ",
+			list = {
+				"●",
+				"➜",
+				"★",
+				"‒",
+			},
+		},
+	},
+})
 require("options")
 require("keymaps")
 require("autocmds")
