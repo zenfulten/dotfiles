67ed2da84a270ea08486a00f89098b9bcc551f1e
Rio <rioten353@gmail.com>
2025-05-10 18:45:08 +0530

    Add: hypridle

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,4 @@
+require("configs.options")
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
@@ -55,6 +56,5 @@ require("lazy").setup({
 		},
 	},
 })
-require("configs.options")
 require("configs.keymaps")
 require("configs.autocmds")
