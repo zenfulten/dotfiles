372db510ecf3714db8229bb9b4b028c53ed098dc
Rio <rioten353@gmail.com>
2025-05-28 12:00:26 +0530

    Config: script

--- hyprland/.config/hypr/scripts/clipboard-manager.sh
+++ hyprland/.config/hypr/scripts/clipboard-manager.sh
@@ -12,10 +12,10 @@
 session_type=$XDG_SESSION_TYPE
 
 if [ "$session_type" == "wayland" ]; then
-    cliphist list | rofi -dmenu -display-columns 2 | cliphist decode | wl-copy
+    cliphist list | rofi -theme ~/.config/rofi/themes/clipboard.rasi -dmenu -display-columns 2 | cliphist decode | wl-copy
 
 elif [ "$session_type" == "x11" ]; then
-    cliphist list | rofi -dmenu -display-columns 2 | cliphist decode | xclip -selection clipboard
+    cliphist list | rofi -theme ~/.config/rofi/themes/clipboard.rasi -dmenu -display-columns 2 | cliphist decode | xclip -selection clipboard
 else
     echo "Тип сеанса не определен или не является Wayland/X11."
 fi
